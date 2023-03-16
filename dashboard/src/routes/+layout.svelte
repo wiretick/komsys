4e80335ad10f86b59b4cb44b03b08e7c097c1781
Sondre Gunneng <s.gunneng@gmail.com>
2023-03-16 15:37:14 +0100

    Basic styling of table and fetching some data to populate it

--- dashboard/src/routes/+layout.svelte
+++ dashboard/src/routes/+layout.svelte
@@ -2,5 +2,13 @@
     import "../app.css";
 </script>
 
-<h1 class="text-2xl">Hello world</h1>
-<slot />
+<header class="bg-white shadow">
+    <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+        <h1 class="text-2xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+    </div>
+</header>
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <slot />
+    </div>
+</main>
--- dashboard/src/routes/+page.svelte
+++ dashboard/src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	export let data;
+</script>
+
+<div class="mx-auto">
+    <table class="table-auto border border-collapse text-left bg-white shadow sm:rounded-lg">
+        <thead class="uppercase text-xs bg-gray-50 text-gray-700">
+            <tr class="border border-collapse">
+                <th class="px-6 py-3">Group</th>
+                <th class="px-6 py-3">Task</th>
+                <th class="px-6 py-3">Status</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each data.groups as { group, task, status }}
+                <tr class="border border-collapse">
+                    <td class="px-6 py-4 text-center">{group}</td>
+                    <td class="px-6 py-4 text-center">{task}</td>
+                    <td class="px-6 py-4">
+                        <span class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">{status}</span>
+                    </td>
+                    <td class="px-6 py-4 text-right">
+                        <a href="#yup" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">On my way</a>
+                    </td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
